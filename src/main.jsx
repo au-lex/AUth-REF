579764b96a631c9c2c99a435fc805b602e75abe6
au-lex <aulexc4d2@gmail.com>
2024-01-06 04:30:54 +0100

    router installation

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,43 @@ import React from 'react';
 import App from './App.jsx';
 import './index.css';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Login from './Pages/Login.jsx';
 import Signup from './Pages/Signup.jsx';
 import Admin from './Pages/Admin.jsx';
 
-const AppRouter = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
-  </Router>
-);
+const router = createBrowserRouter([
+
+  
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/login",
+    element: <Login />,
+  },
+
+  {
+    path: "/signup",
+    element: <Signup />
+  },
+
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+])
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppRouter />
-  </React.StrictMode>
-);
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
