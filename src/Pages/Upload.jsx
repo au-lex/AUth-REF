31afea2f054a2172de343c77d50d991b1fbe330c
au-lex <aulexc4d2@gmail.com>
2024-01-16 09:07:33 +0100

    loader

--- src/Pages/Upload.jsx
+++ src/Pages/Upload.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import { SiCoop } from "react-icons/si";
+import Header from '../Componets/Header';
 const Upload = () => {
-  return (
-    <div>Upload</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
 
-export default Upload
+  useEffect(() => {
+    // Simulate loading by setting a timeout
+    const intervalId = setInterval(() => {
+      setLoading(false); // Set loading to false after the interval
+      clearInterval(intervalId); // Clear the interval to stop it from running continuously
+    }, 2000);
+
+    // Clean up the interval to avoid memory leaks
+    return () => clearInterval(intervalId);
+  }, []); // The empty dependency array ensures that this effect runs once on mount
+
+  return (
+    <>
+      {loading ? (
+        // Show loader while loading is true
+        <div className='bg-slate-900  items-center flex justify-center h-dvh'>
+
+<div className="logo flex pt-[1rem] animate-bounce" >
+  <SiCoop className="text-[2rem] text-orange-600" />
+  <h2 className="text-[1.2rem] text-white font-bold">u-hir</h2>
+</div>
+
+        </div>
+      ) : (
+        // Show profile content once loading is false
+        <>
+<Header />
+          <h2 className='pt-[4rem]'>upload</h2>
+
+          {/* Add the rest of your profile content here */}
+        </>
+      )}
+    </>
+  );
+};
+
+export default Upload;
